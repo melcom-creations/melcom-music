212b310342a017c2dd4d1afac3ef2199ac0e202b
melcom <melcom@gmx.net>
2025-10-31 10:20:57 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -170,7 +170,7 @@ document.addEventListener('DOMContentLoaded', function() {
         function setCookie(name, value, days) {
             let expires = "";
             if (days) {
-                const date = new new Date();
+                const date = new Date();
                 date.setTime(date.getTime() + (days*24*60*60*1000));
                 expires = "; expires=" + date.toUTCString();
             }
