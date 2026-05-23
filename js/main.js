1c3bc24a4684829555b5633ffbf7db4d83c47486
melcom <melcom@gmx.net>
2026-05-23 22:35:42 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -44,6 +44,7 @@ document.addEventListener('DOMContentLoaded', function() {
       'images/header/header-image12.png',
       'images/header/header-image14.png',
 	  'images/header/header-image15.png',
+	  'images/header/header-image16.png',
     ];
     const displayDuration = 10000;
     const fadeDuration = 1000;
